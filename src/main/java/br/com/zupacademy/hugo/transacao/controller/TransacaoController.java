c8398c70808895eab18109a5146337ef9b466cc4
Hugo Oliveira Soares <hugo.soares@zup.com.br>
2021-08-02 21:52:13 -0300

    Consultar compras recentes

--- src/main/java/br/com/zupacademy/hugo/transacao/controller/TransacaoController.java
+++ src/main/java/br/com/zupacademy/hugo/transacao/controller/TransacaoController.java
@@ -1,31 +1,31 @@
 package br.com.zupacademy.hugo.transacao.controller;
 
-import br.com.zupacademy.hugo.transacao.model.Cartao;
-import br.com.zupacademy.hugo.transacao.model.Transactions;
+import br.com.zupacademy.hugo.transacao.model.Transacoes;
+import br.com.zupacademy.hugo.transacao.repository.TransacaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.UUID;
+import java.util.List;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/transacoes")
 public class TransacaoController {
 
-    @GetMapping
-    public ResponseEntity<?> solicitaGerarTransacao(Transactions transactions){
+    @Autowired
+    private TransacaoRepository transacaoRepository;
 
-        RestTemplate restTemplate = new RestTemplate();
-        String url = "http://localhost:7777/api/cartoes";
+    @GetMapping("/{id}")
+    public ResponseEntity<?> buscaComprasRecentes(@PathVariable String id){
 
-        Cartao cartao = new Cartao(UUID.randomUUID().toString(), "hugo@zup.com.br");
+        List<Transacoes> ultimasCompras = transacaoRepository.find10UltimasCompras(id);
+        if (ultimasCompras.isEmpty())
+            return ResponseEntity.notFound().build();
 
-        Cartao cartaoRecebido = restTemplate.postForObject(url, cartao, Cartao.class);
-
-        return ResponseEntity.ok(cartaoRecebido);
+        return ResponseEntity.ok(ultimasCompras);
 
     }
 
